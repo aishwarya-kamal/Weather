3b976d22dee39a127807296b5fae4fe6937f4331
AishwaryaKamal <aishwaryakamal5@gmail.com>
2023-10-04 10:41:48 +0200

    Added weather model

---
+++ app/src/main/java/kamal/aishwarya/weather/model/Weather.kt
@@ -0,0 +1,12 @@
+package kamal.aishwarya.weather.model
+
+import kamal.aishwarya.weather.data.model.WeatherResponse.NetworkCurrentWeather.Condition
+
+data class Weather(
+    val temperature: Double,
+    val wind: Double,
+    val humidity: Int,
+    val feelslikeC: Double,
+    val condition: Condition,
+    val uv: Double,
+)
