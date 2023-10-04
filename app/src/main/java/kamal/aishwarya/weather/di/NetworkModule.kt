027bef6e3762e837a36638f40871c649e0cf340a
AishwaryaKamal <aishwaryakamal5@gmail.com>
2023-10-04 10:40:50 +0200

    Added NetworkModule

---
+++ app/src/main/java/kamal/aishwarya/weather/di/NetworkModule.kt
@@ -0,0 +1,50 @@
+package kamal.aishwarya.weather.di
+
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kamal.aishwarya.weather.BuildConfig
+import kamal.aishwarya.weather.data.network.WeatherApi
+import kamal.aishwarya.weather.utils.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        return if (BuildConfig.DEBUG) {
+            okHttpClient.addInterceptor(httpLoggingInterceptor).build()
+        } else {
+            okHttpClient.build()
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideWeatherApi(retrofit: Retrofit): WeatherApi = retrofit
+        .create(WeatherApi::class.java)
+
+}
