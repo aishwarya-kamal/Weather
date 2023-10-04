65d0fb4a218b1bfb85e47018dc2cea889559c9eb
AishwaryaKamal <aishwaryakamal5@gmail.com>
2023-10-04 10:43:21 +0200

    Added WeatherApplication and planted the timber debug tree for logging

---
+++ app/src/main/java/kamal/aishwarya/weather/WeatherApplication.kt
@@ -0,0 +1,16 @@
+package kamal.aishwarya.weather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
+
+@HiltAndroidApp
+class WeatherApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            plant(Timber.DebugTree())
+        }
+    }
+}
