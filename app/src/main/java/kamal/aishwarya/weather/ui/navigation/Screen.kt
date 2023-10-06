2970ebcedcf932ad1289209c1076d9cf9601ed3f
AishwaryaKamal <aishwaryakamal5@gmail.com>
2023-10-06 23:43:50 +0200

    Removed navigation graph

--- app/src/main/java/kamal/aishwarya/weather/ui/navigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package kamal.aishwarya.weather.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Weather: Screen(route = "Weather")
-    object Forecast: Screen(route = "Forecast")
-}
