1e2f31635856e89d6e40394e9283a094edbb6924
AishwaryaKamal <aishwaryakamal5@gmail.com>
2023-10-06 19:22:27 +0200

    Added missing Repository type

--- app/src/main/java/kamal/aishwarya/weather/di/RepositoryModule.kt
+++ app/src/main/java/kamal/aishwarya/weather/di/RepositoryModule.kt
@@ -3,16 +3,18 @@ package kamal.aishwarya.weather.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import kamal.aishwarya.weather.data.network.WeatherApi
 import kamal.aishwarya.weather.data.repository.DefaultWeatherRepository
+import kamal.aishwarya.weather.data.repository.WeatherRepository
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
 
-    @Provides
     @Singleton
-    fun provideRepository(weatherApi: WeatherApi) =
+    @Provides
+    fun provideRepository(weatherApi: WeatherApi): WeatherRepository =
         DefaultWeatherRepository(weatherApi)
 }
